1ffd7275814c839af73836be13f4c69668a56b4a
nxtperfect <alakaxan@protonmail.com>
2026-09-10 15:21:06 +0200

    fix: move file to cpp standard
    fix: remove unnecessary cstdio include

---
+++ src/main/window.hpp
@@ -0,0 +1,21 @@
+#ifndef WINDOW_H_
+#define WINDOW_H_
+
+#include <GLFW/glfw3.h>
+
+#define GLFW_INCLUDE_NONE
+#define FAILED_GLFW_INITIALIZATION 1;
+#define FAILED_WINDOW_CREATION 2;
+
+const int initialize(const GLFWwindow *window);
+
+void glfw_error_callback(int error, const char *description);
+
+const GLFWwindow *create_window(const int width = 640, const int height = 480,
+                                const char *title = "Hawk");
+
+void set_minimum_opengl_requirements();
+
+void glfw_cleanup();
+
+#endif // WINDOW_H_
